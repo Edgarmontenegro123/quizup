434749e32232491aa0b283d6193741c079e900df
Edgar Montenegro <edgarmontenegro321@gmail.com>
2024-09-08 02:20:39 -0300

    🔚 Finally configurations to start project with tests are ready!

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,12 @@ const config: Config = {
         '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
         '^.+\\.(js|jsx)$': 'babel-jest',
     },
+    globals: {
+        'ts-jest': {
+            tsconfig: 'tsconfig.test.json' // Apunta al archivo tsconfig de test que creaste
+        }
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     testMatch: ["**/src/__tests__/**/*.test.[jt]s?(x)"],
 };
 
