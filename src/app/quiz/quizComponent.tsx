ba3d5350ed82cc3f2501faf78caa7ddeb50004c4
Edgar Montenegro <edgarmontenegro321@gmail.com>
2024-09-10 12:51:20 -0300

    🧩 Create QuizComponent to separate logic (server side) and component rendering (client server)

---
+++ src/app/quiz/quizComponent.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import {QuestionsState} from "@/types/types";
+import Quiz from "@/app/quiz/quiz";
+
+type quizComponentProps = {
+    questions: QuestionsState;
+    totalQuestions: number
+}
+
+const QuizComponent = ({questions, totalQuestions} : quizComponentProps) => {
+    return (
+        <div>
+            <Quiz questions={questions} totalQuestions={totalQuestions} />
+        </div>
+    )
+}
+
+
+export default QuizComponent
+
