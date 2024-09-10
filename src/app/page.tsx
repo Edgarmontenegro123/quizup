b9394987c03bd42513a3bb19d636b8eddb7048f6
Edgar Montenegro <edgarmontenegro321@gmail.com>
2024-09-10 12:45:17 -0300

    💀 Add skeleton to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,62 @@
 "use client"
 
 import {useState} from "react";
+import {useRouter} from "next/navigation";
+import Button from "@/app/components/button/button";
+import Image from "next/Image";
+import Brain from "../app/public/images/brain.png"
+import "./globals.css";
+import {Difficulty} from "@/types/types";
 
 export default function Home() {
-    const [category, setCategory] = useState<string>('General Knowledge')
-    const [difficulty, setDifficulty] = useState<string>('medium')
+    const router = useRouter()
+    const [difficult, setDifficult] = useState('')
 
-    const handlePlay = () => {
-        localStorage.setItem('category', category)
-        localStorage.setItem('difficulty', difficulty)
+    const handleClickDifficult = (difficult: Difficulty) => {
+        setDifficult(difficult)
+        console.log("Nivel seleccionado: ", difficult)
+    }
+    const handleButtonClick = () => {
+        if(difficult) {
+            router.push(`/quiz?difficulty=${difficult}`)
+        }
+        else {
+            alert('Selecciona la dificultad!')
+        }
     }
 
   return (
-      <div className="flex flex-col items-center justify-center min-h-screen p-4">
-          <h1 className="text-3xl font-bold mb-4">QuizUp!</h1>
-          <div className="mb-4">
-              <label htmlFor="category" className="mr-2 text-pink-500">Selecciona la temática:</label>
-              <select
-                  id="category"
-                  value={category}
-                  onChange={(e) => setCategory(e.target.value)}
-                  className="border p-2 text-pink-500"
-                  >Opciones de categoría acá!
-              </select>
+      <div className="text-center">
+          <h1 className="header">QuizUp!</h1>
+          <p className="text">Preparado para convertirte en la nueva leyenda del QuizUp!?</p>
+          <div className="text">Agregar carrusel para elegir categorías!</div>
+          <div className="flex justify-around my-5">
+              <div className="flex flex-col border-l-2 border-blue-700 rounded-md p-4 hover:scale-110 hover:shadow-lg"
+                   onClick={() => handleClickDifficult(Difficulty.EASY)} >
+                  <Image src={Brain} alt="Image"
+                         className="mb-7  mt-1"
+                         width={60}
+                         height={60}/>
+                  <p className="text font-bold">Fácil</p>
+              </div>
+              <div className="flex flex-col border-l-2 border-blue-700 rounded-md p-4 hover:scale-110 hover:shadow-lg"
+                   onClick={() => handleClickDifficult(Difficulty.MEDIUM)} >
+                  <Image src={Brain} alt="Image"
+                         className="mb-5"
+                         width={70}
+                         height={70}/>
+                  <p className="text font-bold">Medio</p>
+              </div>
+              <div className="flex flex-col border-l-2 border-blue-700 rounded-md p-4 hover:scale-110 hover:shadow-lg"
+                   onClick={() => handleClickDifficult(Difficulty.HARD)} >
+                  <Image src={Brain} alt="Image"
+                         className="mb-2"
+                         width={80}
+                         height={80}/>
+                  <p className="text font-bold">Difícil</p>
+              </div>
           </div>
-          <div className="mb-4">
-              <label htmlFor="difficulty"
-                     className="mr-2 text-pink-500">Selecciona la dificultad:</label>
-              <select
-                  id="difficulty"
-                  value={difficulty}
-                  onChange={(e) => setDifficulty(e.target.value)}
-                  className="border p-2 text-pink-500"
-                  >
-                  <option value="easy">Fácil</option>
-                  <option value="medium">Medio</option>
-                  <option value="hard">Difícil</option>
-              </select>
-          </div>
-          <button
-              onClick={handlePlay}
-              className="bg blue-500 text-white px-4 py-2 rounded"
-              >¡Jugar!</button>
+          <Button text={"Comenzar!"} onClick={handleButtonClick}/>
       </div>
   );
 }
