b6a262968d13dbc8dd67ae71fb5e541fd8062039
Edgar Montenegro <edgarmontenegro321@gmail.com>
2024-09-11 19:56:02 -0300

    ♾️ Add Spinner component

--- src/app/quiz/quizComponent.tsx
+++ src/app/quiz/quizComponent.tsx
@@ -3,6 +3,7 @@
 import {Difficulty} from "@/types/types";
 import Quiz from "@/app/quiz/quiz";
 import useTrivia from "@/hooks/useTrivia";
+import Spinner from "@/app/components/spinner/spinner";
 
 type quizComponentProps = {
     difficulty: Difficulty;
@@ -13,7 +14,7 @@ type quizComponentProps = {
 const QuizComponent = ({difficulty, category, totalQuestions} : quizComponentProps) => {
     const {questions, loading, error, fetchTriviaQuestions} = useTrivia({difficulty, category});
 
-    if(loading) return <p className="text">Cargando preguntas...</p>;
+    if(loading) return <Spinner />;
     if(error) return <p className="text text-red-500">{error}</p>
 
     return (
