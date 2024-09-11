60f2aef9cd9ea81cbaa885fdd91ab57414aec3c3
Edgar Montenegro <edgarmontenegro321@gmail.com>
2024-09-10 22:29:46 -0300

    🗺️ Add logic to get categories and pass to other components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,48 @@
 "use client"
 
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import {useRouter} from "next/navigation";
-import Button from "@/app/components/button/button";
-import Image from "next/Image";
-import Brain from "../app/public/images/brain.png"
-import "./globals.css";
 import {Difficulty} from "@/types/types";
+import Brain from "../app/public/images/brain.png"
+import Image from "next/Image";
+import Carrousel from "@/app/components/carrousel/carrousel";
+import Button from "@/app/components/button/button";
+import "./globals.css";
 
 export default function Home() {
     const router = useRouter()
     const [difficult, setDifficult] = useState('')
+    const [categories, setCategories] =  useState([]);
+    const [selectedCategory, setSelectedCategory] = useState<number | null>(null)
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const res = await fetch('https://opentdb.com/api_category.php');
+                const data = await res.json();
+                setCategories(data.trivia_categories);
+            } catch (error) {
+                console.log("Error fetching categories:", error)
+            }
+        }
+        fetchCategories();
+    }, [])
+
+    const handleCategoryClick = (categoryId: number) => {
+        setSelectedCategory(categoryId);
+        console.log("Categoría seleccionada: ", categoryId)
+    };
 
     const handleClickDifficult = (difficult: Difficulty) => {
         setDifficult(difficult)
         console.log("Nivel seleccionado: ", difficult)
     }
     const handleButtonClick = () => {
-        if(difficult) {
-            router.push(`/quiz?difficulty=${difficult}`)
+        if(difficult && selectedCategory) {
+            router.push(`/quiz?category=${selectedCategory}&difficulty=${difficult}`)
         }
         else {
-            alert('Selecciona la dificultad!')
+            alert('Selecciona la dificultad y categoría!')
         }
     }
 
@@ -29,14 +50,18 @@ export default function Home() {
       <div className="text-center">
           <h1 className="header">QuizUp!</h1>
           <p className="text">Preparado para convertirte en la nueva leyenda del QuizUp!?</p>
-          <div className="text">Agregar carrusel para elegir categorías!</div>
+          <div className="text  my-5">
+              <Carrousel categories={categories} onCategorySelect={handleCategoryClick}/>
+          </div>
           <div className="flex justify-around my-5">
               <div className="flex flex-col border-l-2 border-blue-700 rounded-md p-4 hover:scale-110 hover:shadow-lg"
                    onClick={() => handleClickDifficult(Difficulty.EASY)} >
                   <Image src={Brain} alt="Image"
                          className="mb-7  mt-1"
                          width={60}
-                         height={60}/>
+                         height={60}
+                         priority
+                  />
                   <p className="text font-bold">Fácil</p>
               </div>
               <div className="flex flex-col border-l-2 border-blue-700 rounded-md p-4 hover:scale-110 hover:shadow-lg"
@@ -44,7 +69,9 @@ export default function Home() {
                   <Image src={Brain} alt="Image"
                          className="mb-5"
                          width={70}
-                         height={70}/>
+                         height={70}
+                         priority
+                  />
                   <p className="text font-bold">Medio</p>
               </div>
               <div className="flex flex-col border-l-2 border-blue-700 rounded-md p-4 hover:scale-110 hover:shadow-lg"
@@ -52,7 +79,9 @@ export default function Home() {
                   <Image src={Brain} alt="Image"
                          className="mb-2"
                          width={80}
-                         height={80}/>
+                         height={80}
+                         priority
+                  />
                   <p className="text font-bold">Difícil</p>
               </div>
           </div>
